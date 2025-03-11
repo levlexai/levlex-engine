b738896fdb3d60571c3f794dfa98a14bcad020a3
twizzworld <103385201+numinousmuses@users.noreply.github.com>
2025-03-11 15:37:09 -0500

    added LLM prompting utils

---
+++ src/interfaces/internet.ts
@@ -0,0 +1,18 @@
+export interface InternetAgentRequest{
+    prompt: string;
+    n_queries?: number; // default three
+    ak?: number;
+    service?: InternetService;
+    model?: Model
+}
+
+export interface Model{
+    name: string;
+    base_url: string;
+    ak: string;
+}
+
+export interface InternetService{
+    name: 'tavily' | 'jina' | 'brave' | 'exa';
+    ak: string;
+}
